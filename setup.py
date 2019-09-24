27b773ac7924ecf95db9059852d301de128dd94f
Thomas Aarholt <thomasaarholt@gmail.com>
2019-09-24 13:04:48 +0200

    Fix setup.py
    Couldn't install it with the filename different from the inner folder name (pyms)

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="py_multislice",
+    name="pyms",
     version="0.1",
     description="An open-source Python multislice package",
     author="Hamish Brown",
