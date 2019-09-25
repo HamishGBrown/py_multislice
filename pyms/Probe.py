S-matrix calculations introduced and Bug in defocus (factor 2) fixed

pyms/Probe.py
@@ -58,8 +58,8 @@ def q_space_array(pixels, gridsize):
     given by pixels (#y pixels, #x pixels) and real space size given by gridsize
     (y size, x size)"""
     return np.meshgrid(
-        *[np.fft.fftfreq(pixels[i]) * pixels[i] / gridsize[i] for i in range(1, -1, -1)]
-    )[::-1]
+        *[np.fft.fftfreq(pixels[i], d=gridsize[i] / pixels[i]) for i in [1, 0]]
+    )
 
 
 def chi(q, qphi, lam, df=0.0, aberrations=[]):
@@ -74,7 +74,7 @@ def chi(q, qphi, lam, df=0.0, aberrations=[]):
         wavelength of electron (Inverse angstroms).
     aberrations : list
         A list object containing a set of the class aberration"""
-    chi_ = (q * lam) ** 2 / 2 * df / 2
+    chi_ = (q * lam) ** 2 / 2 * df
     for ab in aberrations:
         chi_ += (
             (q * lam) ** (ab.n + 1)
@@ -250,6 +250,7 @@ def wavev(E):
     # Electron rest mass in eV
     m0c2 = 5.109989461e5
     return np.sqrt(E * (E + 2 * m0c2)) / hc
+
 
 def relativistic_mass_correction(E):
     """Gives the relativistic mass correction, m/m0 or gamma, for an electron

pyms/crystal.py
@@ -6,7 +6,7 @@ import copy
 from re import split, match
 from os.path import splitext
 from .atomic_scattering_params import e_scattering_factors, atomic_symbol
-from .Probe import wavev,relativistic_mass_correction
+from .Probe import wavev, relativistic_mass_correction
 from .utils.torch_utils import (
     sinc,
     roll_n,
@@ -17,6 +17,7 @@ from .utils.torch_utils import (
     fourier_shift_array,
     amplitude,
 )
+
 
 def bandwidth_limit_array(array, limit=2 / 3):
     """Band-width limit an array to fraction of its maximum given by limit"""
@@ -47,6 +48,7 @@ def bandwidth_limit_array(array, limit=2 / 3):
 
     return array
 
+
 def Xray_scattering_factor(Z, gsq, units="A"):
     # Bohr radius in Angstrom
     a0 = 0.529177
@@ -54,6 +56,7 @@ def Xray_scattering_factor(Z, gsq, units="A"):
     return Z - 2 * np.pi ** 2 * a0 * gsq * electron_scattering_factor(
         Z, gsq, units=units
     )
+
 
 def electron_scattering_factor(Z, gsq, units="VA"):
     ai = e_scattering_factors[Z - 1, 0:10:2]
@@ -77,6 +80,7 @@ def electron_scattering_factor(Z, gsq, units="VA"):
     elif units == "A":
         return fe
 
+
 def interaction_constant(E, units="rad/VA"):
     """Calculates the interaction constant, sigma, to convert electrostatic
     potential (in V Angstrom) to radians. Units of this constant are rad/(V
@@ -96,7 +100,8 @@ def interaction_constant(E, units="rad/VA"):
         return 2 * np.pi * gamma * me * qe / k0 / h / h
     elif units == "rad/A":
         return gamma / k0
-        
+
+
 def q_space_array(pixels, gridsize):
     """Returns the appropriately scaled 2D reciprocal space array for pixel size
     given by pixels (#y pixels, #x pixels) and real space size given by gridsize
@@ -104,19 +109,34 @@ def q_space_array(pixels, gridsize):
     return np.meshgrid(
         *[np.fft.fftfreq(pixels[i], d=gridsize[i] / pixels[i]) for i in [1, 0]]
     )
-    
-def rot_matrix(theta, u = np.asarray([0,0,1],dtype=np.float)):
-    '''Generates the rotational matrix for a rotation of angle theta in radians
-    around vector u.'''
-    from numpy import sin, cos,pi
+
+
+def rot_matrix(theta, u=np.asarray([0, 0, 1], dtype=np.float)):
+    """Generates the rotational matrix for a rotation of angle theta in radians
+    around vector u."""
+    from numpy import sin, cos, pi
+
     c = cos(theta)
     s = sin(theta)
-    ux,uy,uz = u
-    R = np.zeros((3,3))
-    R[0,:] = [c+ux*ux*(1-c),ux*uy*(1-c)-uz*s,ux*uz*(1-c)+uy*s]
-    R[1,:] = [uy*uz*(1-c)+uz*s,c+uy*uy*(1-c),uy*uz*(1-c)-ux*s]
-    R[2,:] = [uz*ux*(1-c)-uy*s,uz*uy*(1-c)+ux*s,c+uz*uz*(1-c)]
+    ux, uy, uz = u
+    R = np.zeros((3, 3))
+    R[0, :] = [
+        c + ux * ux * (1 - c),
+        ux * uy * (1 - c) - uz * s,
+        ux * uz * (1 - c) + uy * s,
+    ]
+    R[1, :] = [
+        uy * uz * (1 - c) + uz * s,
+        c + uy * uy * (1 - c),
+        uy * uz * (1 - c) - ux * s,
+    ]
+    R[2, :] = [
+        uz * ux * (1 - c) - uy * s,
+        uz * uy * (1 - c) + ux * s,
+        c + uz * uz * (1 - c),
+    ]
     return R
+
 
 class crystal:
     # Elements in a crystal object:
@@ -217,7 +237,7 @@ class crystal:
             return None
         # If temperature factors are given as B then convert to urms
         if temperature_factor_units == "B":
-            self.atoms[:, 4:6] /= 8 * np.pi ** 2
+            self.atoms[:, 5] /= 8 * np.pi ** 2
 
     def quickplot(self, atomscale=0.01, cmap=plt.get_cmap("Dark2")):
         """Makes a quick 3D scatter plot of the crystal"""
@@ -229,28 +249,31 @@ class crystal:
         sizes = self.atoms[:, 3] ** (4) * atomscale
         ax.scatter(*[self.atoms[:, i] for i in range(3)], c=colors, s=sizes)
         plt.show(block=True)
-    
-    def output_vesta_xtl(self,fnam):
+
+    def output_vesta_xtl(self, fnam):
         """Outputs an .xtl file which is viewable by the vesta software:
 
         K. Momma and F. Izumi, "VESTA 3 for three-dimensional visualization of
         crystal, volumetric and morphology data," J. Appl. Crystallogr., 44,
         1272-1276 (2011).
         """
-        f = open(splitext(fnam)[0]+'.xtl', 'w')
-        f.write('TITLE ' + self.Title + '\n')
-        f.write('CELL ' + '\n')
-        f.write('  {0:.2f} {1:.2f} {2:.2f} 90 90 90\n'.format(*self.unitcell))
-        f.write('SYMMETRY NUMBER 1' + '\n')
-        f.write('SYMMETRY LABEL  P1' + '\n')
-        f.write('ATOMS ' + '\n')
-        f.write('NAME         X           Y           Z'+ '\n')
-        for  i in range(self.atoms.shape[0]):
-            f.write('{0} {1:.4f} {2:.4f} {3:.4f}\n'.format(
-            atomic_symbol[int(self.atoms[i,3])],*self.atoms[i,:3]))
-        f.write('EOF')
+        f = open(splitext(fnam)[0] + ".xtl", "w")
+        f.write("TITLE " + self.Title + "\n")
+        f.write("CELL " + "\n")
+        f.write("  {0:.2f} {1:.2f} {2:.2f} 90 90 90\n".format(*self.unitcell))
+        f.write("SYMMETRY NUMBER 1" + "\n")
+        f.write("SYMMETRY LABEL  P1" + "\n")
+        f.write("ATOMS " + "\n")
+        f.write("NAME         X           Y           Z" + "\n")
+        for i in range(self.atoms.shape[0]):
+            f.write(
+                "{0} {1:.4f} {2:.4f} {3:.4f}\n".format(
+                    atomic_symbol[int(self.atoms[i, 3])], *self.atoms[i, :3]
+                )
+            )
+        f.write("EOF")
         f.close()
-    
+
     def make_transmission_functions(
         self,
         pixels,
@@ -494,134 +517,152 @@ class crystal:
 
         # Only return real part
         return torch.ifft(P, signal_ndim=2)[..., 0]
-        
-    def rotate(self, theta,axis,origin = [0.5,0.5,0.5]):
+
+    def rotate(self, theta, axis, origin=[0.5, 0.5, 0.5]):
         """Returns a copy of the crystal rotated an angle theta in radians about an axis and
         """
         new = copy.deepcopy(self)
-        
-        #Make rotation matrix, R, and  the point about which we rotate, O
+
+        # Make rotation matrix, R, and  the point about which we rotate, O
         R = rot_matrix(theta, axis)
         O = np.asarray(origin)
-        
-        #Get atomic coordinates in cartesian (not fractional coordinates)
-        new.atoms[:,:3] = self.atoms[:,:3]*self.unitcell[np.newaxis,:]
-        
-        #Apply rotation matrix to each atom coordinate
-        new.atoms[:,:3] = (R@(new.atoms[:,:3].T - O[:,np.newaxis]) 
-                                                + O[:,np.newaxis]).T
-        
-        #Get new unit cell (assume vacuum padding)
-        origin = np.amin(new.atoms,axis=0)
-        new.unitcell = np.amax(new.atoms,axis=0)-origin
-        new.atoms[:,:3] =  (new.atoms[:,:3] - origin[np.newaxis,:3])/new.unitcell[np.newaxis,:3]
-        
-        #Return rotated crystal
+
+        # Get atomic coordinates in cartesian (not fractional coordinates)
+        new.atoms[:, :3] = self.atoms[:, :3] * self.unitcell[np.newaxis, :]
+
+        # Apply rotation matrix to each atom coordinate
+        new.atoms[:, :3] = (
+            R @ (new.atoms[:, :3].T - O[:, np.newaxis]) + O[:, np.newaxis]
+        ).T
+
+        # Get new unit cell (assume vacuum padding)
+        origin = np.amin(new.atoms, axis=0)
+        new.unitcell = np.amax(new.atoms, axis=0) - origin
+        new.atoms[:, :3] = (new.atoms[:, :3] - origin[np.newaxis, :3]) / new.unitcell[
+            np.newaxis, :3
+        ]
+
+        # Return rotated crystal
         return new
-        
-    def tile(self, x = 1, y = 1, z = 1):
-        '''tiles the crystal out'''
-        #Make copy of original crystal
+
+    def tile(self, x=1, y=1, z=1):
+        """tiles the crystal out"""
+        # Make copy of original crystal
         new = copy.deepcopy(self)
-        
-        #Get atoms in unit cell
+
+        # Get atoms in unit cell
         natoms = self.atoms.shape[0]
-        
-        #Initialize new atom list
-        new.atoms = np.zeros((natoms*x*y*z,3))
-        
-        #Calculate new unit cell size
-        new.unitcell = np.asarray([x,y,z])*self.unitcell
-        
-        #tile out the integer amounts
+
+        # Initialize new atom list
+        new.atoms = np.zeros((natoms * x * y * z, 3))
+
+        # Calculate new unit cell size
+        new.unitcell = np.asarray([x, y, z]) * self.unitcell
+
+        # tile out the integer amounts
         for j in range(int(x)):
             for k in range(int(y)):
                 for l in range(int(z)):
-                    
-                    #Calculate origin of this particular tile
-                    origin = [float(j)/factorx,float(k)/factory,float(l)/factorz]
-                    
-                    #Calculate index of this particular tile
-                    indx = j*int(y)*int(z)+y*int(z)+l
-                    
-                    #Add new atoms to unit cell
-                    new.atoms[indx:indx+int(z),:3] = self.atoms[:,:3] + origin[np.newaxis,:]
-                    new.atoms[indx:indx+int(z),3:] = self.atoms[:,3:]
-                        
-        #Return new crystal
+
+                    # Calculate origin of this particular tile
+                    origin = [
+                        float(j) / factorx,
+                        float(k) / factory,
+                        float(l) / factorz,
+                    ]
+
+                    # Calculate index of this particular tile
+                    indx = j * int(y) * int(z) + y * int(z) + l
+
+                    # Add new atoms to unit cell
+                    new.atoms[indx : indx + int(z), :3] = (
+                        self.atoms[:, :3] + origin[np.newaxis, :]
+                    )
+                    new.atoms[indx : indx + int(z), 3:] = self.atoms[:, 3:]
+
+        # Return new crystal
         return new
-        
-    def concatenate_crystals(self, other,axis = 2,side = 1,eps=1e-3):
-        '''adds other crystal to the crystal object slice is added to the bottom (top being z =0)
+
+    def concatenate_crystals(self, other, axis=2, side=1, eps=1e-3):
+        """adds other crystal to the crystal object slice is added to the bottom (top being z =0)
         only works if slices are the same size or their x and y dimensions are integer multiples of
-        each other '''
-        #Make deep copies of the crystal object and the slice
-        #this is so that these objects remain untouched by the operation
-        #of this function
+        each other """
+        # Make deep copies of the crystal object and the slice
+        # this is so that these objects remain untouched by the operation
+        # of this function
         new = copy.deepcopy(self)
         other_ = copy.deepcopy(other)
-        
-        #Check if the two slices are the same size and
-        #tile accordingly
+
+        # Check if the two slices are the same size and
+        # tile accordingly
         for ax in range(3):
-            #If this axis is the concatenation axis, then it's not necessary 
-            #that the crystals are the same size
-            if ax==axis: continue
-            
-            factor = self.unitcell[ax]/other.unitcell[ax]
-            tile = [0,0,0]
-            
-            if factor > 1 :
+            # If this axis is the concatenation axis, then it's not necessary
+            # that the crystals are the same size
+            if ax == axis:
+                continue
+
+            factor = self.unitcell[ax] / other.unitcell[ax]
+            tile = [0, 0, 0]
+
+            if factor > 1:
                 tile[ax] = int(factor)
                 other_ = other_.tile(*tile)
-            elif factor <1 :
-                tile[ax] = int(1/factor)
+            elif factor < 1:
+                tile[ax] = int(1 / factor)
                 new = new.tile(*tile)
-        
+
         axes = np.arange(3) != axis
-        assert np.all(np.abs(new.unitcell[axes]-other_.unitcell[axes])<eps), "Crystal axes mismatch"
-        
-        #Update the thickness of the resulting
-        #crystal object.
-        new.unitcell[axis] = self.unitcell[axis]+other_.unitcell[axis]
-        
-        #Adjust fractional coordinates of atoms
-        new.atoms[:,axis]   /= new.unitcell[axis]/self.unitcell[axis]
-        other_.atoms[:,axis] /= new.unitcell[axis]/other_.unitcell[axis]
-        
-        if side == 0:  new.atoms[:,axis] += self.unitcell[axis]/new.unitcell[axis]
-        else: other_.atoms[:,axis] += self.unitcell[axis]/new.unitcell[axis]
-        new.atoms = np.concatenate([new.atoms,other_.atoms],axis=0)
-        
+        assert np.all(
+            np.abs(new.unitcell[axes] - other_.unitcell[axes]) < eps
+        ), "Crystal axes mismatch"
+
+        # Update the thickness of the resulting
+        # crystal object.
+        new.unitcell[axis] = self.unitcell[axis] + other_.unitcell[axis]
+
+        # Adjust fractional coordinates of atoms
+        new.atoms[:, axis] /= new.unitcell[axis] / self.unitcell[axis]
+        other_.atoms[:, axis] /= new.unitcell[axis] / other_.unitcell[axis]
+
+        if side == 0:
+            new.atoms[:, axis] += self.unitcell[axis] / new.unitcell[axis]
+        else:
+            other_.atoms[:, axis] += self.unitcell[axis] / new.unitcell[axis]
+        new.atoms = np.concatenate([new.atoms, other_.atoms], axis=0)
+
         return new
-        
-    def reflect(self,axes):
+
+    def reflect(self, axes):
         """Reflect crystal in each of the axes enumerated in list axes"""
         self_ = copy.deepcopy(self)
         for ax in axes:
-            self_.atoms[:,ax] = 1 - self_.atoms[:,ax]
+            self_.atoms[:, ax] = 1 - self_.atoms[:, ax]
         return self_
-    
-    def slice(self,range,axis):
+
+    def slice(self, range, axis):
         """Make a slice of crystal object ranging from range[0] to range[1] through
         specified axis"""
-        
-        #Work out which atoms will stay in the sliced structure
-        mask = np.logical_and(self.atoms[:,axis]>=range[0], self.atoms[:,axis]<=range[1])
-        
-        #Make a copy of the crystal
+
+        # Work out which atoms will stay in the sliced structure
+        mask = np.logical_and(
+            self.atoms[:, axis] >= range[0], self.atoms[:, axis] <= range[1]
+        )
+
+        # Make a copy of the crystal
         new = copy.deepcopy(self)
-        
-        #Put remaining atoms back in 
-        new.atoms = self.atoms[mask,:]
-        
-        #Adjust unit cell dimensions
-        new.unitcell[axis] = (range[1]-range[0])*self.unitcell[axis]
-        
-        #Adjust origin of atomic coordinates
+
+        # Put remaining atoms back in
+        new.atoms = self.atoms[mask, :]
+
+        # Adjust unit cell dimensions
+        new.unitcell[axis] = (range[1] - range[0]) * self.unitcell[axis]
+
+        # Adjust origin of atomic coordinates
         origin = np.zeros((3))
         origin[axis] = range[0]
-        new.atoms[:,axis] = (new.atoms[:,axis] - range[0])*self.unitcell[axis]/new.unitcell[axis]
-        
-        #Return modified crystal structure
+        new.atoms[:, axis] = (
+            (new.atoms[:, axis] - range[0]) * self.unitcell[axis] / new.unitcell[axis]
+        )
+
+        # Return modified crystal structure
         return new

pyms/py_multislice.py
@@ -2,6 +2,7 @@ import numpy as np
 import torch
 import matplotlib.pyplot as plt
 import sys
+from tqdm import tqdm
 from re import split, match
 from os.path import splitext
 from .atomic_scattering_params import e_scattering_factors, atomic_symbol
@@ -25,7 +26,8 @@ def q_space_array(pixels, gridsize):
     return np.meshgrid(
         *[np.fft.fftfreq(pixels[i], d=gridsize[i] / pixels[i]) for i in [1, 0]]
     )
-    
+
+
 def bandwidth_limit_array(array, limit=2 / 3):
     """Band-width limit an array to fraction of its maximum given by limit"""
     if isinstance(array, np.ndarray):
@@ -58,6 +60,7 @@ def bandwidth_limit_array(array, limit=2 / 3):
 
 def make_propagators(pixelsize, gridsize, eV, subslices):
     from .Probe import make_contrast_transfer_function
+
     # We will use the make_contrast_transfer_function function to generate the propagator, the
     # aperture of this propagator will supply the bandwidth limit of our simulation
     # it must be 2/3rds of our pixel gridsize
@@ -74,16 +77,12 @@ def make_propagators(pixelsize, gridsize, eV, subslices):
         # Calculate propagator
         prop[islice, :, :] = bandwidth_limit_array(
             make_contrast_transfer_function(
-                pixelsize,
-                gridsize[:2],
-                eV,
-                app,
-                df=deltaz,
-                app_units="invA"
+                pixelsize, gridsize[:2], eV, app, df=deltaz, app_units="invA"
             )
         )
 
     return prop
+
 
 def multislice_cupy(
     probes,
@@ -91,7 +90,7 @@ def multislice_cupy(
     transmission_functions,
     nslices,
     tiling=None,
-    device_type='GPU',
+    device_type="GPU",
     seed=None,
     return_numpy=True,
 ):
@@ -105,10 +104,10 @@ def multislice_cupy(
     # If GPU calculations are requested then import the cupy library
     # otherwise import numpy (useful feature of numpy and cupy having very
     # the same function names)
-    if device_type=='GPU':
+    if device_type == "GPU":
         import cupy as cp
     else:
-        import numpy as cp 
+        import numpy as cp
 
     # Since pytorch doesn't have a complex data type we need to add an extra
     # dimension of size 2 to each tensor that will store real and imaginary
@@ -127,18 +126,23 @@ def multislice_cupy(
             # Transmit and forward Fourier transform
 
             if tiling is None or (tiling[0] == 1 & tiling[1] == 1):
-                psi = cp.fft.fft2(T[it, subslice, ...]*psi)
+                psi = cp.fft.fft2(T[it, subslice, ...] * psi)
                 # If the transmission function is from a tiled unit cell then
                 # there is the option of randomly shifting it around to
                 # generate "more" transmission functions
             elif nopiy % tiling[0] == 0 and nopix % tiling[1] == 0:
                 # Shift an integer number of pixels in y and x
-                T_ = cp.roll(T[it, subslice, ...],(
-                    r.randint(0, tiling[0]) * (nopiy // tiling[0]),
-                    r.randint(1, tiling[1]) * (nopix // tiling[1])),axis=(-2,-1))
+                T_ = cp.roll(
+                    T[it, subslice, ...],
+                    (
+                        r.randint(0, tiling[0]) * (nopiy // tiling[0]),
+                        r.randint(1, tiling[1]) * (nopix // tiling[1]),
+                    ),
+                    axis=(-2, -1),
+                )
 
                 # Perform transmission operation
-                psi = cp.fft.fft2(T_*psi)
+                psi = cp.fft.fft2(T_ * psi)
             else:
                 # Case of a non-integer shifting of the unit cell
                 yshift = r.randint(0, tiling[0]) * (nopiy / tiling[0])
@@ -165,7 +169,8 @@ def multislice_cupy(
 
     if return_numpy:
         return cx_to_numpy(psi)
-    return psi    
+    return psi
+
 
 def multislice(
     probes,
@@ -258,7 +263,7 @@ def multislice(
 
             # Propagate and inverse Fourier transform
             psi = torch.ifft(complex_mul(psi, P[subslice, ...]), signal_ndim=2)
-
+            # print(torch.sum(amplitude(psi)))
     if return_numpy:
         return cx_to_numpy(psi)
     return psi
@@ -324,7 +329,7 @@ def STEM(
     if scan_posn is None:
 
         # Calculate field of view of scan
-        FOV = np.asarray(rsize) / np.asarray(tiling)
+        FOV = np.asarray(rsize[:2]) / np.asarray(tiling)
 
         # Calculate number of scan positions in STEM scan
         nscan = nyquist_sampling(FOV, eV=eV, alpha=alpha)
@@ -470,7 +475,6 @@ def unit_cell_shift(array, axis, shift, tiles):
        shift than array an integer number of unit cells"""
 
     intshift = array.size(axis) // tiles
-    integer_divisible = intshift == 0
 
     indices = torch.remainder(torch.arange(array.shape[-3 + axis]) - shift)
     if axis == 0:
@@ -490,3 +494,100 @@ def max_grid_resolution(gridshape, rsize, bandwidthlimit=2 / 3, eV=None):
     from .Probe import wavev
 
     return max_res / wavev(eV) * 1e3
+
+
+def make_scattering_matrix(
+    rsize,
+    propagators,
+    transmission_functions,
+    nslices,
+    eV,
+    alpha,
+    batch_size=1,
+    device=None,
+    PRISM_factor=[1, 1],
+    tiling = [1,1],
+    device_type=None,
+    seed=None,
+    showProgress=True,
+    mode="stream",
+):
+    """Make a scattering matrix for the dynamical scattering calculations using 
+    the PRISM algorithm"""
+    
+    from .Probe import wavev, plane_wave_illumination
+
+    # Get size of grid
+    gridshape = np.shape(propagators)[1:3]
+
+    # Datatype (precision) is inferred from transmission functions
+    dtype = transmission_functions.dtype
+
+    # Device (CPU or GPU) is also inferred from transmission functions
+    if device is None:
+        device = transmission_functions.device
+
+    # Get alpha in units of inverse Angstrom
+    alpha_ = wavev(eV) * alpha * 1e-3
+
+    # Make a list of beams in the scattering matrix
+    q = q_space_array(gridshape, rsize)
+    beams = np.argwhere(np.logical_and(
+        np.logical_and(
+            np.less_equal(q[0] ** 2 + q[1] ** 2, alpha_),
+            (
+                np.mod(
+                    np.fft.fftfreq(gridshape[0], d=1 / gridshape[0]).astype(np.int),
+                    PRISM_factor[0],
+                )
+                == 0
+            )[:, np.newaxis],
+        ),
+        (
+            np.mod(
+                np.fft.fftfreq(gridshape[1], d=1 / gridshape[1]).astype(np.int),
+                PRISM_factor[1],
+            )
+            == 0
+        )[np.newaxis, :],
+    ))
+    nbeams = beams.shape[0]
+
+    # Initialize scattering matrix...
+    if mode == "stream":
+        # ... on CPU for streaming
+        S = torch.zeros(nbeams, *gridshape, 2, dtype=dtype, device="cpu")
+    else:
+        # ... on specified device otherwise
+        S = torch.zeros(nbeams, *gridshape, 2, dtype=dtype, device=device)
+
+    if seed is None:
+        # If no seed passed to random number generator then make one to pass to
+        # the multislice algorithm. This ensure that each column in the scattering
+        # matrix sees the same frozen phonon configuration
+        seed = np.random.randint(0, 2 ** 32 - 1)
+
+    # Initialize probe wave function array
+    psi = torch.zeros(batch_size, *gridshape, 2, dtype=dtype, device=device)
+    
+    for i in tqdm(range(int(np.ceil(nbeams / batch_size))), disable=not showProgress):
+        for j in range(batch_size):
+            jj = j + batch_size * i
+            psi[j, ...] = cx_from_numpy(plane_wave_illumination(
+                gridshape, rsize[:2], tilt=beams[jj, :], tilt_units="pixels"
+            ))
+        psi = multislice(
+            psi,
+            propagators,
+            transmission_functions,
+            nslices,
+            tiling,
+            device,
+            seed,
+            return_numpy=False,
+        )
+        if mode == 'stream': 
+            S[i * batch_size : (i + 1) * batch_size, ...] = psi.cpu()
+        else:
+            S[i * batch_size : (i + 1) * batch_size, ...] = psi
+    return S,beams