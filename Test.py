Bug fix for interpolation normalization of diffraction patterns

Test.py
@@ -13,7 +13,9 @@ scratch_cell_name = "test_cell.xyz"
 
 def sumsqr(array):
     """Calculate the sum of squares for a single array."""
-    return np.sum(np.square(array))
+    if torch.is_tensor(array):
+        return torch.sum(pyms.utils.amplitude(array))
+    return np.sum(np.square(np.abs(array)))
 
 
 def sumsqr_diff(array1, array2):
@@ -77,6 +79,33 @@ def make_temp_structure(atoms=None, title="scratch", ucell=None, seed=0):
 class Test_util_Methods(unittest.TestCase):
     """Test the utility functions for numpy and pytorch."""
 
+    def test_torch_complex_matmul(self):
+        """Test complex matmul against numpy complex matrix multiplication."""
+        k, m, n = 2, 3, 4
+        a = np.random.randint(-5, 5, size=k * m).reshape(
+            (k, m)
+        ) + 1j * np.random.randint(-5, 5, size=k * m).reshape((k, m))
+        b = np.random.randint(-5, 5, size=m * n).reshape(
+            (m, n)
+        ) + 1j * np.random.randint(-5, 5, size=m * n).reshape((m, n))
+
+        c = sumsqr_diff(
+            a @ b,
+            pyms.utils.cx_to_numpy(
+                pyms.utils.complex_matmul(
+                    *[pyms.utils.cx_from_numpy(x) for x in [a, b]]
+                )
+            ),
+        )
+        self.assertTrue(c < 1e-10)
+
+    def test_torch_complex_mul(self):
+        """Multiply 1 + i and 3 + 4i to give -1 + 7 i."""
+        a = torch.as_tensor([1, 1])
+        b = torch.as_tensor([3, 4])
+        c = pyms.utils.complex_mul(a, b)
+        self.assertTrue(float(sumsqr_diff(c, torch.as_tensor([-1, 7]))) < 1e-10)
+
     def test_numpy_fft_shift(self):
         """Test to see if fourier shift correctly shifts a pixel 2 to the right."""
         test_array = np.zeros((5, 5))
@@ -89,27 +118,53 @@ class Test_util_Methods(unittest.TestCase):
             and sumsqr_diff(shifted1, test_array) < 1e-10
         )
 
+    def test_torch_c_exp(self):
+        """Test exponential function by calculating e^{i pi} and e^{1 + i pi}."""
+        # Test e^{i pi} = -1
+        # Test complex input
+        arg = np.asarray(np.pi + 1j)
+        a = pyms.utils.torch_c_exp(pyms.utils.cx_from_numpy(arg))
+        passcomplex = (a.data[0] + 1 / np.exp(1.0)) ** 2 < 1e-10 and a.data[
+            1
+        ] ** 2 < 1e-10
+
+        # Test real input
+        arg = torch.as_tensor([np.pi])
+        a = pyms.utils.torch_c_exp(arg)
+        passreal = (a[0, 0] + 1) ** 2 < 1e-10
+        passtest = passreal and passcomplex
+        self.assertTrue(passtest.item())
+
     def test_crop(self):
         """Test cropping method on scipy test image."""
         # Get astonaut image
         from skimage.data import astronaut
 
-        img = np.sum(astronaut(), axis=2).astype(np.float32)
+        im = np.sum(astronaut(), axis=2).astype(np.float32)
 
-        # Check that normal cropping works
-        cropPass = (
-            sumsqr_diff(
-                pyms.utils.crop(img, [256, 256]), img[128 : 512 - 128, 128 : 512 - 128]
+        passTest = True
+
+        for c_func, img in zip(
+            [pyms.utils.crop, pyms.utils.crop_torch],
+            [im, torch.as_tensor(copy.deepcopy(im))],
+        ):
+            # Check that normal cropping works
+            cropPass = (
+                sumsqr_diff(
+                    c_func(img, [256, 256]), img[128 : 512 - 128, 128 : 512 - 128]
+                )
+                < 1e-10
             )
-            < 1e-10
-        )
+            passTest = passTest and cropPass
 
-        # If an output size larger than that of the input is requested then the
-        # input array should be padded instead, check that this is working too.
-        pad = pyms.utils.crop(img, [700, 700])
-        pad[350 - 256 : 350 + 256, 350 - 256 : 350 + 256] -= img
-        padPass = sumsqr(pad) < 1e-10
-        self.assertTrue(padPass and cropPass)
+            # If an output size larger than that of the input is requested then the
+            # input array should be padded instead, check that this is working too.
+            pad = c_func(img, [700, 700])
+            pad[350 - 256 : 350 + 256, 350 - 256 : 350 + 256] -= img
+            padPass = sumsqr(pad) < 1e-10
+            passTest = passTest and padPass
+
+        self.assertTrue(passTest)
 
     def test_fourier_interpolation(self):
         """Test fourier interpolation of a cosine function."""
@@ -131,7 +186,16 @@ class Test_util_Methods(unittest.TestCase):
             < 1e-10
         )
 
-        numpyVersionPass = passY and passX
+        # Test that the option 'conserve_norm' works too.
+        passNorm = (
+            np.sum(
+                pyms.utils.fourier_interpolate_2d(a, (8, 8), norm="conserve_L2") ** 2
+            )
+            - np.sum(a ** 2)
+            < 1e-10
+        )
+
+        numpyVersionPass = (passY and passX) and passNorm
 
         # test pytorch versions
         passY = (
@@ -149,7 +213,19 @@ class Test_util_Methods(unittest.TestCase):
             - 1 / np.sqrt(2)
             < 1e-10
         )
-        pytorchVersionPass = passY.item() and passX.item()
+
+        # Test that the option 'conserve_norm' works too.
+        passNorm = (
+            torch.sum(
+                pyms.utils.fourier_interpolate_2d_torch(
+                    pyms.utils.cx_from_numpy(a.T), (8, 8), correct_norm=False
+                )
+                ** 2
+            )
+            - torch.sum(pyms.utils.cx_from_numpy(a) ** 2)
+            < 1e-10
+        )
+        pytorchVersionPass = passY.item() and passX.item() and passNorm.item()
 
         self.assertTrue(pytorchVersionPass and numpyVersionPass)
 
@@ -441,6 +517,43 @@ class Test_py_multislice_Methods(unittest.TestCase):
 
         self.assertTrue(nyquistpass and probe_scanpass)
 
+    def test_diffraction_pattern_resize(self):
+        """Test the method for resizing diffraction patterns."""
+        from skimage.data import astronaut
+
+        im = np.fft.fftshift(np.sum(astronaut(), axis=2).astype(np.float32))
+        im /= np.sum(im)
+        # Assume that the original pattern measures 1 x 1 inverse Angstroms and is
+        # 512 x 512 (size of astronaut image)
+        rsize = im.shape
+        gridshape = im.shape
+        # Check cropping only
+        FourD_STEM = [[256, 256]]
+        gridout, resize = pyms.workout_4DSTEM_datacube_DP_size(
+            FourD_STEM, rsize, gridshape
+        )
+
+        imout1 = resize(im)
+
+        # imout1 should just be im cropped by factor of two
+        passtest = (
+            sumsqr_diff(pyms.utils.crop(np.fft.ifftshift(im), FourD_STEM[0]), imout1)
+            < 1e-10
+        )
+
+        # Check cropping  and interpolation
+        FourD_STEM = [[512, 512], [1.5, 1.5]]
+
+        gridout, resize = pyms.workout_4DSTEM_datacube_DP_size(
+            FourD_STEM, rsize, gridshape
+        )
+
+        imout2 = resize(im)
+
+        # Make sure that imout2 has the same normalisation as bfore
+        passtest = sumsqr_diff(np.sum(imout2), np.sum(im)) < 1e-10 and passtest
+        self.assertTrue(passtest)
+
     def test_Smatrix(self):
         """
         Testing scattering matrix and PRISM algorithms.
@@ -581,7 +694,7 @@ class Test_py_multislice_Methods(unittest.TestCase):
         )
 
         probe = torch.squeeze(probe).flatten(0, 1)
-        new = torch.zeros(np.prod(gridout), 2, dtype=S.dtype, device=S.device)
+        new = torch.zeros(np.prod(gridout), 2, dtype=S.dtype, device=probe.device)
         new[window] = probe[window]
         probe = new.reshape(*gridout, 2)
         probe = torch.fft(probe, signal_ndim=2) / np.sqrt(np.prod(gridout))
@@ -606,6 +719,16 @@ class Test_py_multislice_Methods(unittest.TestCase):
 
 
 if __name__ == "__main__":
+
+    # import matplotlib.pyplot as plt
+
+    # fig,ax = plt.subplots(nrows=3)
+    # ax[0].imshow(np.fft.fftshift(im))
+    # ax[1].imshow(imout1)
+    # ax[2].imshow(imout2)
+    # print([np.sum(x) for x in [im,imout1,imout2]])
+    # plt.show(block=True)
+    # sys.exit()
     unittest.main()
     clean_temp_structure()
     # sys.exit()

pyms/py_multislice.py
@@ -23,6 +23,7 @@ from .utils.torch_utils import (
     size_of_bandwidth_limited_array,
     fourier_interpolate_2d_torch,
     crop_window_to_flattened_indices_torch,
+    crop_torch,
 )
 
 
@@ -307,6 +308,7 @@ def multislice(
     return psi
 
 
+# TODO make detectors binary for use with numpy and pytorch sum routines
 def make_detector(gridshape, rsize, eV, betamax, betamin=0, units="mrad"):
     """
     Make a STEM detector with acceptance angle between betamin and betamax.
@@ -477,7 +479,7 @@ def workout_4DSTEM_datacube_DP_size(FourD_STEM, rsize, gridshape):
             # diffraction patterns
             def resize(array):
                 cropped = crop(np.fft.fftshift(array, axes=(-1, -2)), diff_pat_crop)
-                return fourier_interpolate_2d(cropped, gridout)
+                return fourier_interpolate_2d(cropped, gridout, norm="conserve_L1")
 
         else:
             # Define resampling function to just crop diffraction
@@ -510,7 +512,7 @@ def STEM(
     datacube=None,
     scan_posn=None,
     dtype=torch.float32,
-    device=torch.device("cpu"),
+    device=None,
     tiling=[1, 1],
     seed=None,
     showProgress=True,
@@ -594,8 +596,14 @@ def STEM(
     # Get number of thicknesses in the series
     nthick = len(nslices)
 
+    if device is None:
+        device = get_device(device)
+
     # Get shape of grid
-    gridshape = probe.shape[-2:]
+    if torch.is_tensor(probe):
+        gridshape = probe.shape[-3:-1]
+    else:
+        gridshape = probe.shape[-2:]
 
     # Generate scan positions in units of pixels if not supplied
     if scan_posn is None:
@@ -748,7 +756,6 @@ def max_grid_resolution(gridshape, rsize, bandwidthlimit=2 / 3, eV=None):
     max_res = min([gridshape[x] / rsize[x] / 2 * bandwidthlimit for x in range(2)])
     if eV is None:
         return max_res
-    from .Probe import wavev
 
     return max_res / wavev(eV) * 1e3
 
@@ -765,7 +772,7 @@ class scattering_matrix:
         eV,
         alpha,
         GPU_streaming=False,
-        batch_size=1,
+        batch_size=30,
         device=None,
         PRISM_factor=[1, 1],
         tiling=[1, 1],
@@ -845,8 +852,6 @@ class scattering_matrix:
             space view than that implied by the multislice at no cost to
             computational accuracy.
         """
-        from .Probe import wavev
-
         # Get size of grid
         gridshape = transmission_functions.size()[-3:-1]
 
@@ -855,7 +860,9 @@ class scattering_matrix:
 
         # Device (CPU or GPU) is also inferred from transmission functions
         self.device = device
-        if self.device is None:
+        if GPU_streaming:
+            self.device = torch.device("cpu")
+        elif self.device is None:
             self.device = transmission_functions.device
 
         # Get alpha in units of inverse Angstrom
@@ -959,6 +966,7 @@ class scattering_matrix:
             transmission_functions,
             subslicing=subslicing,
             showProgress=self.show_Progress,
+            batch_size=batch_size,
         )
 
     def Propagate(
@@ -1152,8 +1160,9 @@ class scattering_matrix:
                 )
                 self.S[beams] = output
 
-    def PRISM_crop_window(self, win=None):
+    def PRISM_crop_window(self, win=None, device=None):
         """Calculate 2D array indices of STEM crop window."""
+        device = get_device(device)
         if win is None:
             win = self.PRISM_factor
 
@@ -1161,6 +1170,7 @@ class scattering_matrix:
             torch.arange(
                 -self.stored_gridshape[i] // (2 * win[i]),
                 self.stored_gridshape[i] // (2 * win[i]),
+                device=device,
             )
             for i in range(2)
         ]
@@ -1189,14 +1199,14 @@ class scattering_matrix:
         """
         from copy import deepcopy
 
-        crop_ = self.PRISM_crop_window()
-
         if Smat is None:
             Smat = self.S
         Sshape = Smat.shape
 
+        device = Smat.device
+        crop_ = self.PRISM_crop_window(device=device)
         # Ensure posn and probes are pytorch arrays
-        probes = ensure_torch_array(probes, dtype=self.dtype, device=self.device)
+        probes = ensure_torch_array(probes, dtype=self.dtype, device=device)
 
         # Ensure probes tensors correspond to the shape N x Y x X x 2
         # If they have the shape Y x X x 2 then reshape to 1 x Y x X x 2
@@ -1207,9 +1217,9 @@ class scattering_matrix:
         nprobes = probes.shape[0]
 
         if posn is None:
-            posn = torch.zeros(nprobes, 2, device=self.device, dtype=self.dtype)
+            posn = torch.zeros(nprobes, 2, device=device, dtype=self.dtype)
         else:
-            posn = torch.as_tensor(posn, device=self.device, dtype=self.dtype).view(
+            posn = torch.as_tensor(posn, device=device, dtype=self.dtype).view(
                 (nprobes, 2)
             )
 
@@ -1263,7 +1273,7 @@ class scattering_matrix:
             # Flatten the array dimensions
             flattened_shape = [Smat.size(-3) * Smat.size(-2), 2]
             output = torch.zeros(
-                probes.size(0), *flattened_shape, dtype=self.dtype, device=self.device
+                probes.size(0), *flattened_shape, dtype=self.dtype, device=Smat.device
             )
 
             # For evaluating the probes in real space we only want to perform the matrix
@@ -1287,13 +1297,8 @@ class scattering_matrix:
                     Smat.view(self.nbeams, *flattened_shape)[:, window],
                 ) / np.sqrt(np.prod(probes.size()[-3:-1]))
 
-                # output[k,window] = 1
-
-            output = cx_from_numpy(
-                crop(
-                    cx_to_numpy(output.reshape(probes.size(0), *Smat.size()[-3:])),
-                    self.stored_gridshape,
-                )
+            output = crop_torch(
+                output.reshape(probes.size(0), *Smat.size()[-3:]), self.stored_gridshape
             )
 
             return torch.fft(output, signal_ndim=2)
@@ -1381,6 +1386,7 @@ class scattering_matrix:
             aberrations=aberrations,
             app_units="invA",
         )
+        probe = cx_from_numpy(probe, device=device, dtype=self.dtype)
 
         # Make scan positions if none already provided
         if scan_posns is None:
@@ -1449,7 +1455,9 @@ class scattering_matrix:
         # markersize=1.0,color=c)
         # Loop over probe positions clusters. This would be a good candidate
         # for multi-GPU work.
-        for cluster in clusters:
+        for cluster in tqdm(
+            clusters, desc="Probe position clusters", disable=not showProgress
+        ):
             # for c,cluster in zip(colors,clusters):
 
             # Get map of probe positions in cluster
@@ -1537,11 +1545,9 @@ class scattering_matrix:
             # Define a function that will map probe positions for the global
             # scattering matrix to their correct place on the smaller scattering
             # matrix streamed to the GPU.
-            gshape = (
-                torch.as_tensor(self.stored_gridshape).to(self.device).type(self.dtype)
-            )
-            Origin = torch.as_tensor([ymin, xmin]).to(self.device).type(self.dtype)
-            segment_size = torch.as_tensor(size).to(self.device).type(self.dtype)
+            gshape = torch.as_tensor(self.stored_gridshape).to(device).type(self.dtype)
+            Origin = torch.as_tensor([ymin, xmin]).to(device).type(self.dtype)
+            segment_size = torch.as_tensor(size).to(device).type(self.dtype)
 
             def scan_transform(posn):
                 return (posn * gshape - Origin) / segment_size
@@ -1564,7 +1570,7 @@ class scattering_matrix:
                 scan_posn=scan_posns[points],
                 STEM_image=STEM_image_segment,
                 method_kwargs=kwargs,
-                showProgress=showProgress,
+                showProgress=False,
                 device=device,
             )
 

pyms/utils/numpy_utils.py
@@ -161,13 +161,23 @@ def colorize(z):
     return c
 
 
-def fourier_interpolate_2d(ain, shapeout):
+def fourier_interpolate_2d(ain, shapeout, norm="conserve_val"):
     """
     Perfom fourier interpolation on array ain so that its shape matches shapeout.
 
-    Arguments:
-    ain      -- Input numpy array
-    shapeout -- Shape of output array
+    Arguments
+    ---------
+    ain      -- array_like
+        Input numpy array
+    shapeout -- int (2,) , array_like
+        Shape of output array
+
+    Keyword arguments
+    -----------------
+    norm     -- str
+        Normalization of output. If 'conserve_val' then array values are preserved
+        if 'conserve_norm' L2 norm is conserved under interpolation and if
+        'conserve_L1' L1 norm is conserved under interpolation
     """
     # Import required FFT functions
     from numpy.fft import fft2, ifft2
@@ -186,7 +196,13 @@ def fourier_interpolate_2d(ain, shapeout):
     aout[..., maskout] = fft2(np.asarray(ain, dtype=np.complex))[..., maskin]
 
     # Fourier transform result with appropriate normalization
-    aout = ifft2(aout) * (1.0 * np.prod(shapeout) / np.prod(np.shape(ain)[-2:]))
+    if norm == "conserve_val":
+        aout = ifft2(aout) * (np.prod(shapeout) / np.prod(np.shape(ain)[-2:]))
+    elif norm == "conserve_norm":
+        aout = ifft2(aout) * np.sqrt(np.prod(shapeout) / np.prod(np.shape(ain)[-2:]))
+    else:
+        aout = ifft2(aout)
+
     # Return correct array data type
     if (
         str(ain.dtype) in ["float64", "float32", "float", "f"]

pyms/utils/torch_utils.py
@@ -94,6 +94,7 @@ def torch_c_exp(angle):
     else:
         # Case of a complex valued exponent
         exp = torch.exp(-angle[im])
+        result = torch.zeros(*angle.shape, dtype=angle.dtype, device=angle.device)
         result[re] = exp * torch.cos(angle[re])
         result[im] = exp * torch.sin(angle[re])
     return result
@@ -105,20 +106,22 @@ def sinc(x):
     return torch.sin(np.pi * y) / np.pi / y
 
 
-def ensure_torch_array(array, dtype=torch.float, device=torch.device("cpu")):
+def ensure_torch_array(array, dtype=torch.float, device=None):
     """
     Ensure that the input array is a pytorch tensor.
 
     Converts to a pytorch array if input is a numpy array and do nothing if the
     input is a pytorch tensor
     """
+    if device is None:
+        device = get_device(device)
     if not isinstance(array, torch.Tensor):
         if np.iscomplexobj(array):
             return cx_from_numpy(array, dtype=dtype, device=device)
         else:
             return torch.from_numpy(array).type(dtype).to(device)
     else:
-        return array
+        return array.to(device)
 
 
 def modulus_square(r):
@@ -481,3 +484,50 @@ def fourier_interpolate_2d_torch(
     if inputComplex:
         return aout
     return aout[re]
+
+
+def crop_torch(arrayin, shapeout):
+    """
+    Crop the last two dimensions of arrayin to grid size shapeout.
+
+    For entries of shapeout which are larger than the shape of the input array,
+    perform zero-padding.
+    """
+    C = iscomplex(arrayin)
+
+    # Number of dimensions in input array
+    ndim = arrayin.ndim
+
+    # Number of dimensions not covered by shapeout (ie not to be cropped)
+    nUntouched = ndim - 2 - C
+
+    # Shape of output array
+    shapeout_ = arrayin.shape[:nUntouched] + tuple(shapeout)
+    if C:
+        shapeout_ += (2,)
+
+    arrayout = torch.zeros(shapeout_, dtype=arrayin.dtype, device=arrayin.device)
+
+    y, x = arrayin.shape[-2 - C :][:2]
+    y_, x_ = shapeout[-2:]
+
+    def indices(y, y_):
+        if y > y_:
+            # Crop in y dimension
+            y1, y2 = [(y - y_) // 2, (y + y_) // 2]
+            y1_, y2_ = [0, y_]
+        else:
+            # Zero pad in y dimension
+            y1, y2 = [0, y]
+            y1_, y2_ = [(y_ - y) // 2, (y + y_) // 2]
+        return y1, y2, y1_, y2_
+
+    y1, y2, y1_, y2_ = indices(y, y_)
+    x1, x2, x1_, x2_ = indices(x, x_)
+
+    if C:
+        arrayout[..., y1_:y2_, x1_:x2_, :] = arrayin[..., y1:y2, x1:x2, :]
+    else:
+        arrayout[..., y1_:y2_, x1_:x2_] = arrayin[..., y1:y2, x1:x2]
+
+    return arrayout